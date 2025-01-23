20c9f82b569d26a9009481ebed2180148779f312
toobahaider001 <159890263+toobahaider001@users.noreply.github.com>
2025-01-23 21:27:36 +0500

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,26 @@ import Hero from "@/components/ui/Hero";
 import Pick from "@/components/ui/Pick";
 import Product from "@/components/ui/Product";
 import { productType } from "@/types/products";
-
 export default async function Home() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-  if (!res.ok) throw new Error('Failed to fetch products');
-  const data: productType[] = await res.json();
+  let data: productType[] = [];
+
+  if (process.env.NODE_ENV !== 'production') {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+      if (!res.ok) throw new Error('Failed to fetch products');
+      data = await res.json();
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  }
+
   return (
-    <><Hero />
+    <>
+      <Hero />
       <Pick />
       <Product products={data} />
       <Classic />
       <Featured />
-
     </>
   );
 }
