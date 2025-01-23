71f83f1a09ce4c9a22ffe901170727438657f64b
toobahaider001 <159890263+toobahaider001@users.noreply.github.com>
2025-01-23 16:09:59 +0500

    hackathon completed

--- src/app/actions/query.ts
+++ src/app/actions/query.ts
@@ -1,24 +1,24 @@
 import { client } from "@/sanity/lib/client";
 import { productType } from "@/types/products";
 
-export const getCart = async () => {
+export  const getCart = async ()=>{
   const query = `
-*[_type == "order"]{
-  _id,
-  title,
-  description,
-  productImage{
-    asset->{
-      _id,
-      url
-    }
-  },
-  price,
-  tags,
-  discountPercentage,
-  isNew
-}
+  *[_type == "order"]{
+    _id,
+    title,
+    description,
+    productImage{
+      asset->{
+        _id,
+        url
+      }
+    },
+    price,
+    tags,
+    discountPercentage,
+    isNew
+  }
 `;
-  const data: productType[] = await client.fetch(query);
-  return data;
-};
+const data:productType[] =await client.fetch(query);
+return data
+}
